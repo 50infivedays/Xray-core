68c0e7a59dfbbc53a72e927b94dad63da3697729
stonewu <wlinpisces@gmail.com>
2023-11-27 14:45:27 +0800

    add horizon

---
+++ proxy/horizon/errors.generated.go
@@ -0,0 +1,9 @@
+package horizon
+
+import "github.com/xtls/xray-core/common/errors"
+
+type errPathObjHolder struct{}
+
+func newError(values ...interface{}) *errors.Error {
+	return errors.New(values...).WithPathObj(errPathObjHolder{})
+}
